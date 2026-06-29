6aaa52e3758b62ef57b19bd107a441574d1097bd
Leetram <theomarteel59310@gmail.com>
2026-06-29 12:44:38 +0200

    ESLint ruleset changes 1

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,5 +3,17 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["src/module/**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  { 
+    files: ["src/module/**/*.{js,mjs,cjs}"], 
+    plugins: { js }, 
+    extends: ["js/recommended"], 
+    languageOptions: { 
+      globals: globals.browser
+    },
+    rules: {
+			"no-unused-vars": "warn",
+      "no-undef": "off",
+      "no-useless-escape": "warn"
+    } 
+  },
 ]);
