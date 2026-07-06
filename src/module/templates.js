b780cc7e5e2cc2e77b3f938d035133b3eea37592
Joshua Martin <joshuamartintx@gmail.com>
2026-07-05 22:55:31 -0500

    Added bio tab
    This push adds a biography tab to the character sheet, localization for the field names, and persistent data fields in the character data.

--- src/module/templates.js
+++ src/module/templates.js
@@ -15,9 +15,11 @@
     "systems/projectmoonttrpg/templates/sheet/character/partials/tab-main.hbs",
     "systems/projectmoonttrpg/templates/sheet/character/partials/attributes.hbs",
     "systems/projectmoonttrpg/templates/sheet/character/partials/augment.hbs",
+    "systems/projectmoonttrpg/templates/sheet/character/partials/bios.hbs",
     "systems/projectmoonttrpg/templates/sheet/character/partials/tab-weapons.hbs",
     "systems/projectmoonttrpg/templates/sheet/character/partials/tab-outfits.hbs",
-    "systems/projectmoonttrpg/templates/sheet/character/partials/tab-skills.hbs"
+    "systems/projectmoonttrpg/templates/sheet/character/partials/tab-skills.hbs",
+    "systems/projectmoonttrpg/templates/sheet/character/partials/tab-bios.hbs"
   ];
 
   // Load the template parts
